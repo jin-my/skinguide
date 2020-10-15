3ded08a98f8be4632fc27734e3b16ab292a90439
jin_my <kgssss18@gmail.com>
2020-10-15 23:16:44 +0900

    Database 작업 중

--- skin/models.py
+++ skin/models.py
@@ -5,12 +5,21 @@ from skinguide import settings
 
 class Product(models.Model):
     objects = models.Manager()
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=150)
+    brand = models.CharField(max_length=100, null = True)
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, related_name="write")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    price = models.IntegerField(null = True)
+    skintype = models.CharField(max_length=5, null = True)
+    categories = models.CharField(max_length=100, null = True)
+    gender = models.CharField(max_length=2, null = True) #남/여 or M/F
     body = models.TextField()
-    # writer = models.ForeignKey('user.User', on_delete = models.CASCADE)
+    # example_choice = (
+    #     ('S', 'Small'),
+    #     ('M', 'Medium'),
+    #     ('L', 'Large'),
+    # )
 
     def __str__(self):
         return self.title
--- user/models.py
+++ user/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from skinguide import settings
 # Create your models here.
 class User(AbstractUser):
-    pass
+    # id password last_login is_superuser username first_name last_name email is_staff is_activate date_joined
+    skintype=models.CharField(max_length=5, null = True)
+    gender=models.CharField(max_length=2, null = True)
