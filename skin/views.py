ddb28dd393c9c76a702c3e32821435ac8df426bc
Mei <clara_31@naver.com>
2020-10-21 13:39:26 +0900

    add skincheck

--- skin/views.py
+++ skin/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def skin(request):
     return render(request, 'skintest.html')
+
+def skincheck(request):
+    return render(request, 'skincheck.html')
