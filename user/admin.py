d6db616b558060b52154a2347acf3a97ccb0af1a
jin_my <kgssss18@gmail.com>
2020-10-21 20:30:06 +0900

    database 수정 201021

--- user/admin.py
+++ user/admin.py
@@ -3,13 +3,13 @@ from .models import User
 # Register your models here.
 admin.site.register(User)
 
-from django.contrib import admin
-from .models import Product, Photo
+# from django.contrib import admin
+# from .models import Product, Photo
 
-class ProductInline(admin.TabularInline):
-    model = Product
+# class ProductInline(admin.TabularInline):
+#     model = Product
 
-class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductInline, ]
-# Register your models here.
-admin.site.register(User, ProductAdmin)
+# class ProductAdmin(admin.ModelAdmin):
+#     inlines = [ProductInline, ]
+# # Register your models here.
+# admin.site.register(User, ProductAdmin)
