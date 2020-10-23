82a1fffa60ffded0ceff4d79c8d77ca6633bd890
hgo641 <65850682+hgo641@users.noreply.github.com>
2020-10-23 18:10:50 +0900

    first

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from skinguide import settings
+from django import forms
+#from django.contrib.auth.models import User
 # Create your models here.
 class User(AbstractUser):
-    # id password last_login is_superuser username first_name last_name email is_staff is_activate date_joined
+    # id password last_login is_superuser username first_name last_name email is_staff is_activate date_joinedㄴ
+    #username = models.CharField(max_length = 10)
     skintype=models.CharField(max_length=5, null = True)
     gender=models.CharField(max_length=2, null = True)
+
